3501debf849ba93a70d9951021f040cb59ab294f
Pasa673__ <pasangt6721@gmail.com>
2026-01-25 18:33:29 +0545

    day:1 added tailwind and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#1e40af',
+        accent: '#fbbf24',
+        'text-dark': '#1f2937',
+        'text-light': '#6b7280',
+        'bg-light': '#f3f4f6',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
